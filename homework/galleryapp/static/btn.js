b3e207a5afbb0b13187938e3b5d069a8855dc89d
AmalRajB <ramal37204@gmail.com>
2025-10-13 22:35:10 +0530

    django work 1

---
+++ homework/galleryapp/static/btn.js
@@ -0,0 +1,6 @@
+const h4 = document.getElementsByTagName('h4')[0];
+const btn = document.getElementById('mybtn');
+
+btn.addEventListener("click", function() {
+    h4.innerHTML = "Thankyou For Visiting....";
+});
